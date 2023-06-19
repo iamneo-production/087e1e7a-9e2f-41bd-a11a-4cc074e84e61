df74203d21891f9c495f6662cebd8daf5fb032f6
priyankathunduri434 <priyankathunduri2002@gmail.com>
2023-06-19 06:47:49 +0000

    OrderModel and UserOrderController

---
+++ dotnetapp/WebApp/Models/OrderModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class OrderModel
+    {
+        public int orderId { get; set; }
+      
+        public string orderName { get; set; }
+        public string orderDescription { get; set; }
+
+        public List<ThemeModel> themeModel { get; set; }
+
+        public GiftModel giftModel { get; set; } = new GiftModel();
+
+        public DateTime orderDate { get; set; }
+        public int orderPrice { get; set; }
+        public string orderAddress { get; set; }
+        public string orderPhone { get; set; }
+        public string orderEmail { get; set; }
+        public string orderQuantity { get; set; }
+    }
+}
