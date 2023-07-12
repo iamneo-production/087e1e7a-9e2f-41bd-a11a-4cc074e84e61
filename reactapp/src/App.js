ff37e27af9294accb61beafb96d57fff60d5fde0
Suneel-9199 <saisuneelpotula1@gmail.com>
2023-07-12 11:20:52 +0000

    Admin Themes,Admin Review,User Review

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import Homepage from "./components/customer/Homepage";
+import AdminGifts from "./components/Admin/Admingifts";
+import AdminThemes from "./components/Admin/Adminthemes";
+import Myorders from "./components/customer/Myorders";
+import AdminViewOrders from "./components/Admin/Adminvieworders";
+import Login from "./components/customer/Login";
+import Register from "./components/customer/Signup";
+import { Admin, User } from "./components/customer/Auth";
+import Placeorder  from "./components/customer/Placeorder";
+import ReviewForm from "./components/customer/Review";
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+    <Router>
+    <Routes>
+        <Route  path="/" element={<Login/>} />
+        <Route path="/Signup" element={<Register/> }/>
+        
+        <Route element={<User/>}>
+        <Route path="/Homepage" element={<Homepage/>} />
+        <Route path="/placeorder" element={<Placeorder />} />
+        <Route path="/Myorders" element={<Myorders/> } />
+        <Route path="/review" element={<ReviewForm/>}/>
+        </Route>
+
+        <Route element={<Admin/>}>
+        <Route path="/Admingifts" element={ <AdminGifts/> } />
+        <Route path="/Adminthemes" element={<AdminThemes/> } />
+        <Route path="/Adminvieworders" element={<AdminViewOrders/> } />
+        </Route>
+
+    </Routes>
+    </Router>  
+    
   );
 }
 
