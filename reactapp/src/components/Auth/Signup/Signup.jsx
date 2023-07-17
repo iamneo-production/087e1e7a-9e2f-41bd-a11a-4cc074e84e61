1fe7aff8114b477de106c3ddfbae5b4bd58f4160
SivaSatwikaReddy <sivasatwika.reddy@gmail.com>
2023-07-17 14:09:34 +0000

    fix security

--- reactapp/src/components/Auth/Signup/Signup.jsx
+++ reactapp/src/components/Auth/Signup/Signup.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import './Signup.css'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import {Button ,Row ,Col ,Form} from "react-bootstrap";
+import {Button ,Row ,Col ,Form,InputGroup, FormControl} from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FaPhone,FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+
 
 const Signup=()=>{
   const [userName, setuserName] = useState('');
@@ -14,6 +17,23 @@ const Signup=()=>{
   const [mobileNumber, setMobileNumber] = useState('');
   const [errors, setErrors] = useState({});
   const [userRole, setUserRole] = useState("User");
+
+  const [passwordError,setPasswordError] = useState('');
+
+  const [showPassword, setShowPassword] = useState(false);
+
+// Function to toggle password visibility
+const togglePasswordVisibility = () => {
+  setShowPassword(!showPassword);
+};
+
+const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+// Function to toggle confirm password visibility
+const toggleConfirmPasswordVisibility = () => {
+  setShowConfirmPassword(!showConfirmPassword);
+};
+
 
   const navigate = useNavigate();
   const validateForm = () => {
@@ -26,15 +46,18 @@ const Signup=()=>{
     // Email validation
     if (!email) {
       errors.email = 'Email is required';
-    } else if  (!/\S+@\S+\.\S+/.test(email)) {
+    } else if (!/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/.test(email)){
       errors.email = 'Invalid email address';
     }
     // Password validation
     if (!password) {
       errors.password = 'Password is required';
-    } else if (password.length < 6) {
-      errors.password = 'Password should be at least 6 characters long';
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/.test(password)) {
+      errors.password = 'Invalid Password. Must contain at least one number, one uppercase letter, one lowercase letter, one special character (!@#$%^&*), and be at least 8 characters long';
+    } else {
+      setPasswordError(''); // Clear the password error message
     }
+    
     // Confirm password validation
     if (!confirmPassword) {
       errors.confirmPassword = 'Please confirm your password';
@@ -64,7 +87,7 @@ const Signup=()=>{
           userrole : userRole,
       }
         
-        const url = "https://8080-aeeceaafaebbabadfbbdfdacbcefeddcbcbaffb.project.examly.io/user/addUser";
+        const url = "https://8080-fbfbaaaeabebabafdabadfbbdfdacbcefeddcbcbaffb.project.examly.io/user/addUser";
         axios.post(url,data).then((result)=>{
           console.log(result.data);
           if(result.data === "User Added" || result.data==="user exists"||result.data==='Admin Added'){
@@ -103,10 +126,13 @@ const Signup=()=>{
         {errors.userRole && <span>{errors.userRole}</span>}
         </Form.Group>
 
-        <Form.Group className="mb-3" >      
-        <Form.Control  type="text" id="email" data-testid="email" placeholder=" Enter Email" onChange={(e) => setEmail(e.target.value)} />
-        {errors.email && <span>{errors.email}</span>}
-        </Form.Group>
+        <Form.Group className="mb-3">
+      <InputGroup>
+       <InputGroup.Text> <FiMail /> </InputGroup.Text>
+        <Form.Control  type="text" id="email" data-testid="email" placeholder=" Enter Email" onChange={(e) => setEmail(e.target.value)} required />
+        </InputGroup>
+       {errors.email && <span>{errors.email}</span>}
+      </Form.Group>
 
         <Form.Group className="mb-3" >
         <Form.Control type="text" id="username"  data-testid="username" placeholder=" Enter Username"  onChange={(e) => setuserName(e.target.value)} required />
@@ -114,18 +140,50 @@ const Signup=()=>{
         </Form.Group>
 
         <Form.Group className="mb-3" >
-        <Form.Control type="text" id="mobileNumber"  data-testid="mobileNumber" placeholder=" Enter Mobile Number"  onChange={(e) => setMobileNumber(e.target.value)} required />
-        {errors.mobileNumber && <span>{errors.mobileNumber}</span>}
+        <InputGroup className="mb-3">
+       <InputGroup.Text><FaPhone /></InputGroup.Text>
+         <Form.Control type="text" id="mobileNumber"  data-testid="mobileNumber" placeholder=" Enter Mobile Number"  onChange={(e) => setMobileNumber(e.target.value)} required />
+       </InputGroup> 
+       {errors.mobileNumber && <span>{errors.mobileNumber}</span>}   
         </Form.Group>
 
-        <Form.Group className="mb-3" >
-        <Form.Control  type="password" id="password"  data-testid="password" placeholder="Enter Password"  onChange={(e) => setPassword(e.target.value)} required />
-        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
-        </Form.Group>
+        <Form.Group className="mb-3">
+  <InputGroup>
+    <FormControl
+      type={showPassword ? 'type' : 'password'}
+      id="password"
+      data-testid="password"
+      placeholder="Password"
+      onChange={(e) => setPassword(e.target.value)}
+      required
+    />
+    <InputGroup.Text
+      onClick={togglePasswordVisibility}
+      style={{ cursor: 'pointer' }}
+    >
+      {showPassword ? <FaEyeSlash /> : <FaEye />}
+    </InputGroup.Text>
+  </InputGroup>
+  {errors.password && <span>{errors.password}</span>}
+</Form.Group>
 
-        <Form.Group className="mb-3" >
-        <Form.Control type="password" id="confirmPassword"  data-testid="confirmPassword" placeholder=" Confirm Password"   onChange={(e) => setConfirmPassword(e.target.value)} required />
-        </Form.Group>
+<Form.Group className="mb-3">
+  <InputGroup>
+    <FormControl
+      type={showConfirmPassword ? 'type' : 'password'} id="confirmPassword" data-testid="confirmPassword"
+      placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} required/>
+    <InputGroup.Text
+      onClick={toggleConfirmPasswordVisibility}
+      style={{ cursor: 'pointer' }}
+    >
+      {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+    </InputGroup.Text>
+  </InputGroup>
+  {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+</Form.Group>
+
+          {passwordError && <span>{passwordError}</span>} {/* Display password error message */}
+
         <div className="d-grid ">
             <Button variant="success" type="submit"  data-testid="submitButton" onClick={handleSubmit}  id="submitButton" >Submit</Button>
         </div>
